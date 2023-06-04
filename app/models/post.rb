555dd91bb574c98cf3f83d5a0185f64f8c581d01
ianK-code <kian99564@gmail.com>
2023-06-04 22:10:23 +0300

    Validating some input field

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5, maximum: 100}
+    validates :description, presence: true, length: { minimum: 5, maximum: 500}
+    validates :keywords, presence: true, length: { minimum: 5, maximum: 100}
 end
