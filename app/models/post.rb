50529ac741931f91e20844ac88fcd7557afaefcb
ianK-code <kian99564@gmail.com>
2023-06-05 13:00:19 +0300

    Adding file upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5, maximum: 100}
     validates :description, presence: true, length: { minimum: 5, maximum: 500}
     validates :keywords, presence: true, length: { minimum: 5, maximum: 100}
+
+    has_many_attached :images
 end
